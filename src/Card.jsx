f1c4b47957f623effdfd12ea45f33eb76b39ca2c
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-05-29 12:17:08 +0000

    Mildly improved the look of the card.

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,22 @@
-import './card.css'
+import "./card.css";
 
-export const Card = ({
-  suit="heart", value=4, onClick=()=>{}
-}) => {
+export const Card = ({ suit = "heart", value = 4, onClick = () => {} }) => {
   //  if (value === "K") {
- //   value = 13;
- // } else if (value === "Q") {
-   // value = 12;
-//  } else if (value === "J") {
+  //   value = 13;
+  // } else if (value === "Q") {
+  // value = 12;
+  //  } else if (value === "J") {
   //  value = 11;
- // } else if (value === "A") {
+  // } else if (value === "A") {
   //  value = 1;
   //} else {
-  //  value = value;} 
-  
+  //  value = value;}
+
   return (
     <div className="card" onClick={onClick}>
-      {suit} {value} {onClick}
+      <div class="num top">{value} </div>
+      <div class="value">{suit[0]}</div>
+      <div class="num bottom">{value} </div>
     </div>
-  )
-}
+  );
+};
