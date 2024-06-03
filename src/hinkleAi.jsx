0f855d431ffc6ce17e903bed8e0dc78c0be4e3c5
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-06-03 14:28:36 +0000

    Simpler AI

--- src/hinkleAi.jsx
+++ src/hinkleAi.jsx
@@ -1,21 +1,9 @@
 export const hinkleAi = {
-  name: "Hinkle Rules", // a cute name
+  name: "Hinkle", // a cute name
   icon: "-", // an image link
   getNextCard: (hand, targets, opponentPlays) => {
-    console.log("The hand is:", hand);
-    console.log("The targets are:", targets);
-    console.log("Opponent plays are: ", opponentPlays);
-    let nextTarget = targets[targets.length - 1];
-    if (nextTarget > 10) {
-      // Find my highest card and play it...
-      let max = hand[0];
-      for (let i=1; i<hand.length; i++) {
-        if (hand[i] > max) {
-          max = hand[i];
-        }
-      }
-      return max;
-    }
-    return hand[0];
+    // Play a random card!
+    let index = Math.floor(Math.random() * hand.length);
+    return hand[index];
   },
 };
