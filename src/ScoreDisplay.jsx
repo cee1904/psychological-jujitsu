358bdf7a4c0f7505cc61c5696957cb6ba84d1441
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-05-20 12:52:56 +0000

    Highlight latest card

--- src/ScoreDisplay.jsx
+++ src/ScoreDisplay.jsx
@@ -1,24 +1,45 @@
+import { useState, useRef, useEffect } from "react";
 import { Card } from "./Card";
 
 // Sam and Tristan
 
-export const ScoreDisplay = ({ values = [], suit= "hearts"}) => {
+export const ScoreDisplay = ({ values = [], suit = "hearts" }) => {
+  const [justUpdated, setJustUpdated] = useState(false);
   let sum = 0;
-  values.forEach(
-    (v)=>{
-      sum = sum + v;
+  values.forEach((v) => {
+    sum = sum + v;
+  });
+  const prevSum = useRef(sum);
+  useEffect(() => {
+    if (prevSum.current !== sum) {
+      setJustUpdated(true);
+      prevSum.current = sum;
+    } else {
+      setJustUpdated(false);
     }
-  )
-  
+  }, [sum]);
   return (
-    <div style={{display:"flex"}}>
-      <div style={{position:"relative", width:400}}> 
-      {values.map(
-      (value,idx)=><div style={{left:30*idx, position:"absolute"}}><Card value={value} suit={suit}/></div>
-)
-}
-</div>
-<div>Score: {sum}</div>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <div style={{ position: "relative", width: 400 }}>
+        {values.map((value, idx) => (
+          <div
+            style={{ left: 30 * idx, position: "absolute" }}
+            key={value}
+            className={
+              justUpdated && idx === values.length - 1
+                ? "card-updated card-container"
+                : "card-container"
+            }
+          >
+            <Card value={value} suit={suit} />
+          </div>
+        ))}
+      </div>
+      <div className={justUpdated ? "score score-updated" : "score"}>{sum}</div>
     </div>
   );
 };
